a1b7bafefdbe5c462b4166d80b5f408cf729940d
Ayeni-Olakunle <67742586+Ayeni-Olakunle@users.noreply.github.com>
2021-06-22 08:45:18 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,81 @@
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
+let button4 = document.querySelector("#button4");
+let button5 = document.querySelector("#button5");
+let button6 = document.querySelector("#button6");
+let button7 = document.querySelector("#button7");
+let button8 = document.querySelector("#button8");
+let button9 = document.querySelector("#button9");
+
+button1.addEventListener("click", () => {
+  button1.innerHTML = "X";
+});
+
+button1.addEventListener("dblclick", () => {
+  button1.innerHTML = "O";
+});
+
+button2.addEventListener("click", () => {
+  button2.innerHTML = "X";
+});
+
+button2.addEventListener("dblclick", () => {
+  button2.innerHTML = "O";
+});
+
+button3.addEventListener("click", () => {
+  button3.innerHTML = "X";
+});
+
+button3.addEventListener("dblclick", () => {
+  button3.innerHTML = "O";
+});
+
+button4.addEventListener("click", () => {
+  button4.innerHTML = "X";
+});
+
+button4.addEventListener("dblclick", () => {
+  button4.innerHTML = "O";
+});
+
+button5.addEventListener("click", () => {
+  button5.innerHTML = "X";
+});
+
+button5.addEventListener("dblclick", () => {
+  button5.innerHTML = "O";
+});
+
+button6.addEventListener("click", () => {
+  button6.innerHTML = "X";
+});
+
+button6.addEventListener("dblclick", () => {
+  button6.innerHTML = "O";
+});
+
+button7.addEventListener("click", () => {
+  button7.innerHTML = "X";
+});
+
+button7.addEventListener("dblclick", () => {
+  button7.innerHTML = "O";
+});
+
+button8.addEventListener("click", () => {
+  button8.innerHTML = "X";
+});
+
+button8.addEventListener("dblclick", () => {
+  button8.innerHTML = "O";
+});
+
+button9.addEventListener("click", () => {
+  button9.innerHTML = "X";
+});
+
+button9.addEventListener("dblclick", () => {
+  button9.innerHTML = "O";
+});
